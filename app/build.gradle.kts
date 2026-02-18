317b3565eb971c29175b93456fef684c8df741b5
Your Name <you@example.com>
2026-02-18 14:02:23 -0600

    -Incremented version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.peerloomllc.satscream"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
