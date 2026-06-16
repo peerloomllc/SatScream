c0e320712e6ee48e410f7a4c1a5fc2c28d5e4618
Your Name <you@example.com>
2026-06-16 15:14:16 -0500

    Release v1.0.5 (versionCode 5)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.peerloomllc.satscream"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
