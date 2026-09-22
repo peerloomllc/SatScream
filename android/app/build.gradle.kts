fae52ec9c8a00473d37ef2f3be7f9f1e3a11b809
Your Name <you@example.com>
2026-09-22 11:02:20 -0500

    Release v1.0.9 (versionCode 7)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 7
-        versionName = "1.0.8"
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
